95290e21d7b8e6c80f3ba56f3cad2553b4aedfdd
test <test@test.com>
2023-12-02 13:00:30 +0300

    add files

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.10-slim
+
+WORKDIR /app
+
+
+COPY hellodapi.py .
+
+COPY requirements.txt .
+
+RUN pip install -r requirements.txt
+
+ENTRYPOINT [ "uvicorn", "hellodapi:app", "--host", "0.0.0.0", "--port", "6000"]
---
+++ hellodapi.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# read get post delete update +...
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/test")
+async def test():
+    return {"Hello": "Api Fast"}

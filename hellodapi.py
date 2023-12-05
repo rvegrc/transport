b1064c16ca9623e5fa162412a531f984744ffeae
test <test@test.com>
2023-12-05 22:36:29 +0300

    fix fastapi

--- hellodapi.py
+++ hellodapi.py
@@ -34,6 +34,9 @@ sklearn.set_config(transform_output='pandas')
 # load metrics
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 
+from typing import List
+import typing
+
 pd.set_option('display.float_format', '{:.3f}'.format)
 
 # constant
@@ -42,13 +45,16 @@ RAND_ST = 345
 model = joblib.load('model_test.joblib')
 
 
-class PredOut(BaseModel):
-    distance: float
+class DfRow(BaseModel):
+    distance: int
     own_container: int
     complect_send: int
     container_train: int
     transportation_type: int
     days: int
+
+class DataF(BaseModel):
+    data: List[DfRow]
 
 
 app = FastAPI()
@@ -60,9 +66,13 @@ async def root():
 
 
 @app.post("/predict")
-async def predict(data:PredOut):
-    predictions = model.predict(data)
-    return  predictions # recive dataset, make prediction, return prediction,
+async def predict(data:DataF):
+    data_dict = data.dict()
+    # print(data_dict['data'])
+    print(pd.DataFrame(data_dict['data']))
+    # print(pd.read_json({data_dict['data'], orient='records'))
+        # predictions = model.predict(data)
+    return  {'yes': 'yes'} # predictions # recive dataset, make prediction, return prediction,
 
 @app.get("/test")
 async def test():

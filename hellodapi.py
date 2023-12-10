1d36810a87c95be6a274b8a1ddf6fb5fe22fedca
test <test@test.com>
2023-12-10 08:36:18 +0300

    new model save

--- hellodapi.py
+++ hellodapi.py
@@ -16,6 +16,8 @@ import joblib
 import xgboost as xgb
 import optuna
 
+import json
+
 from optuna.visualization.matplotlib import plot_param_importances
 
 import mlflow
@@ -43,7 +45,7 @@ pd.set_option('display.float_format', '{:.3f}'.format)
 # constant
 RAND_ST = 345
 
-model = joblib.load('model_test.joblib')
+model = joblib.load('best_model.joblib')
 
 
 class DfRow(BaseModel):
@@ -71,12 +73,15 @@ class DfOut(BaseModel):
 
 class XtestOut(BaseModel):
     '''xtest output format'''
-    distance: int
-    own_container: int
-    complect_send: int
-    container_train: int
-    transportation_type: int
-    days: int
+    distance: list
+    own_container: list
+    complect_send: list
+    container_train: list
+    transportation_type: list
+    days: list
+
+class DFOut2(BaseModel):
+    data: XtestOut
 
 
 
@@ -91,6 +96,17 @@ class SerialOut(BaseModel):
     data: int
 
 
+class XtestDout(BaseModel):
+    '''xtest out from dict form to_dict'''
+    distance: dict
+    own_container: dict
+    complect_send: dict
+    container_train: dict
+    transportation_type: dict
+    days: dict
+
+
+
 app = FastAPI()
 
 
@@ -98,15 +114,6 @@ app = FastAPI()
 async def root():
     return {"message": "Model prediction"}
 
-
-@app.post("/predict")
-async def predict(data:DataF):
-    data_dict = data.dict()
-    # print(data_dict['data'])
-    print(pd.DataFrame(data_dict['data']))
-    # print(pd.read_json({data_dict['data'], orient='records'))
-        # predictions = model.predict(data)
-    return  {'yes': 'yes'} # predictions # recive dataset, make prediction, return prediction,
 
 
 @app.post('/text')
@@ -117,22 +124,41 @@ async def train(test:TextOut):
     return {'text': test}
 
 
-@app.post('/xout')
+@app.post('/xout_list')
 async def serial(data:XtestOut):
     'output second item of serias'
     print(data)
+    # print(pd.DataFrame(json.loads(data.json())))
+    # print(model.predict(pd.DataFrame(data)))
+    return data
+
+@app.post('/xout_dict')
+async def xout_dict(data:XtestDout):
+    print(data.json())
+    # print(pd.DataFrame(json.load(data.json())))
     return data
 
 
-# @app.post('/serial')
-# async def serial(data:SerialOut):
-#     'output second item of serias'
-#     return data[1]
+
+
+@app.post("/predict")
+async def predict(data:XtestOut):
+    X_test_out = pd.DataFrame(json.loads(data.json()))
+    predict_x_test_out = model.predict(X_test_out)
+    # print(json.dumps(predict_x_test_out.tolist()))
+    print(pd.Series(predict_x_test_out))
+    # df_data = pd.DataFrame(json.loads(data.json()))
+    # print(df_data)
+    # return  json.dumps(predict_x_test_out.tolist()) # predictions # recive dataset, make prediction, return prediction,
+    return  predict_x_test_out.tolist()
+
 
 
 @app.get("/test")
 async def test():
     return {"Hello": "Api Fast"}
+
+
 
 @app.get("/train/{item}")
 async def train(item):

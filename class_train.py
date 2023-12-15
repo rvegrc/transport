b09fcf172f0f23cb3703d24f80a682ec70cb6cd3
test <test@test.com>
2023-12-15 15:40:53 +0300

    add subclasses to insect

--- class_train.py
+++ class_train.py
@@ -17,13 +17,7 @@ class Insect:
     def data_print(self):
         print(self.name, self.weight, self.hight, self.speed, self.n_legs, self.coord)
 
-
-        # дочерний класс, наследование создать и применить летающие и ползающие
-
-
-class Fly(Insect):
-
-    def fly(self, target_coord, time):
+    def distance_coords(self, target_coord, time):
         distance = self.speed * time
         distance_target = (
             (target_coord[0] - self.coord[0])**2 
@@ -32,34 +26,38 @@ class Fly(Insect):
             ) ** 0.5
         if distance >= distance_target:
             self.coord = target_coord
-            print (self.coord, distance)
+            print (self.name, self.coord, distance_target)
             return 
         koef = distance / distance_target
-        # self.coord = [target_coord[0]*koef, target_coord[1]*koef, target_coord[2]*koef]
+        self.coord = [target_coord[0]*koef, target_coord[1]*koef, target_coord[2]*koef]
+        print (self.name, self.coord, distance)
 
+        # дочерний класс, наследование создать и применить летающие и ползающие
+
+
+class Fly(Insect):
+    pass
+    # n_wings: int
+
+    # def __init__(self, n_wings):
+    #     self.n_wings = n_wings
 
 class Crawl(Insect):
-    def crawl(self, target_coord, time):
-        distance = self.speed * time
-        distance_to_target = ( 
-            (target_coord[0]**2 + target_coord[1]**2)**0.5
-            -(self.coord[0]**2 + self.coord[1]**2)**0.5
-        )
-        if distance >= distance_to_target:
-            self.cood = target_coord
-            return
+    pass
 
 
+mosquito = Fly(name='mosquito', weight=1.2, hight=1.3, speed=21.4, n_legs=6, coord=[0, 0, 0])
 
+spider = Crawl(name='spider', weight=2.2, hight=4.3, speed=1.4, n_legs=8, coord=[0, 0, 0])
 
+mosquito.data_print() 
 
-mosquito = Insect(name='mosquito', weight=1.2, hight=1.3, speed=21.4, n_legs=6, coord=[0, 0, 0])
+# mosquito.fly([8, 1, 5], 6)
 
-spider = Insect(name='spider', weight=2.2, hight=4.3, speed=1.4, n_legs=8, coord=[0, 0, 0])
+spider.data_print()
 
-# mosquito.data_print() 
+spider.distance_coords([8, 1, 0], 2)
 
-mosquito.fly([8, 1, 5], 6)
-
+mosquito.distance_coords([8, 1, 6], 2)
 
 

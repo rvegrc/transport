11a4d822919f475fe8b702f0a07d57c2e8558765
test <test@test.com>
2023-12-24 13:00:36 +0300

    docker compose and nginx

---
+++ ui.py
@@ -0,0 +1,21 @@
+import streamlit as st  #+ sidebar
+import pandas as pd
+import numpy as np
+import requests
+
+
+
+st.title('Uber pickups in NYC') # +session state 
+
+file_upload = st.file_uploader(label = 'Upload_csv')
+process_file = st.button(label="Process_file")
+
+if process_file:
+    result = requests.post('http://localhost:8000/list_predict', json = pd.read_csv(file_upload).to_dict(orient='list'))
+    result_csv = pd.DataFrame(result.json()).to_csv(index=False)
+
+    
+    st.download_button(label='Download_csv', data=result_csv, file_name='predict.csv')
+
+   
+
